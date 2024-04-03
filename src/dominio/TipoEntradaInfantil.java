022c5a0b14bf8381a6d333deeb09eff67f834cba
Silva-Carlos-Andres <silva_carlos_andres@hotmail.com>
2024-04-03 00:09:04 -0300

    ClaseInfantil

---
+++ src/dominio/TipoEntradaInfantil.java
@@ -0,0 +1,30 @@
+package dominio;
+import java.math.BigDecimal;
+
+import enums.EdadesEntradaInfantil;
+import interfaces.ITipoEntrada;
+
+public class TipoEntradaInfantil implements ITipoEntrada {
+	private final static BigDecimal costoMenores = BigDecimal.valueOf(250);
+	private final static BigDecimal costoMayores = new BigDecimal(500);
+	private final static boolean souvenur = true;
+
+	
+	// gets y sets
+	public static BigDecimal getCostoMenores8() {
+		return costoMenores;
+	}
+
+	public static BigDecimal getCostoMayores8() {
+		return costoMayores;
+	}
+	
+	public static boolean getSouvenir() {
+		return souvenur;
+	}
+	
+	@Override
+	public String devuelveInformacion() {
+		return "Entrada Infantil";
+	}
+}
---
+++ src/enums/EdadesEntradaInfantil.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum EdadesEntradaInfantil {
+	Mayores8,
+	Menores8
+
+}
